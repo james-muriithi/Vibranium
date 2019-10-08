22d1450191feea5f4ba961feb402b70fa2805982
james-muriithi <muriithijames556@gmail.com>
2019-10-09 00:42:39 +0300

    fcm

--- gulpfile.js
+++ gulpfile.js
@@ -1,106 +1,106 @@
 'use strict'
 
-const gulp          = require('gulp');
-const css          = require('gulp-clean-css');
-const sourcemaps    = require('gulp-sourcemaps');
-const connect       = require('gulp-connect');
-const htmlmin       = require('gulp-htmlmin');
-const uglify        = require('gulp-uglify-es').default;
+const gulp = require('gulp');
+const css = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+const connect = require('gulp-connect');
+const htmlmin = require('gulp-htmlmin');
+const uglify = require('gulp-uglify-es').default;
 
 const paths = {
-  html: 'src/*.html',
-  css: 'src/css/**/*.css',
-  script: 'src/js/**/*.js',
-  sw: 'src/service-worker.js',
-  images: 'src/images/**',
-  vendor: 'src/js/vendors/*.js',
-  manifest: 'src/manifest.json'
+    html: '*.html',
+    css: 'src/css/**/*.css',
+    script: 'src/js/**/*.js',
+    sw: 'service-worker.js',
+    images: 'src/images/**',
+    vendor: 'src/js/vendors/*.js',
+    manifest: 'manifest.json'
 };
 
 const imagemin = require('gulp-imagemin');
- 
+
 const imagesGulp = function() {
-  return gulp.src(paths.images)
-    .pipe(imagemin([
-      imagemin.gifsicle({interlaced: true}),
-      imagemin.jpegtran({progressive: true}),
-      imagemin.optipng({optimizationLevel: 5}),
-      imagemin.svgo({
-          plugins: [
-              {removeViewBox: true},
-              {cleanupIDs: false}
-          ]
-      })
-    ]))
-    .pipe(gulp.dest('dist/images'));
+    return gulp.src(paths.images)
+        .pipe(imagemin([
+            imagemin.gifsicle({ interlaced: true }),
+            imagemin.jpegtran({ progressive: true }),
+            imagemin.optipng({ optimizationLevel: 5 }),
+            imagemin.svgo({
+                plugins: [
+                    { removeViewBox: true },
+                    { cleanupIDs: false }
+                ]
+            })
+        ]))
+        .pipe(gulp.dest('./public/dist/images'));
 };
 
 const gulpCss = function() {
-  return gulp.src(paths.css)
-    .pipe(sourcemaps.init())
-    .pipe(css())
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('dist/css'));
+    return gulp.src(paths.css)
+        .pipe(sourcemaps.init())
+        .pipe(css())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./public/dist/css'));
 };
 
 const jsGulp = function() {
-  return gulp.src(paths.script, {
-      ignore: [paths.sw, paths.vendor]
-    })
-    .pipe(sourcemaps.init())
-    .pipe(uglify())
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('dist/js'));
+    return gulp.src(paths.script, {
+            ignore: [paths.sw, paths.vendor]
+        })
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./public/dist/js'));
 };
 
 const copyManifest = function() {
-  return gulp.src(paths.manifest)
-      .pipe(gulp.dest('./'))
+    return gulp.src(paths.manifest)
+        .pipe(gulp.dest('./public'))
 };
 
 const swGulp = function() {
-  return gulp.src(paths.sw)
-    .pipe(uglify())
-    .pipe(gulp.dest('./'));
+    return gulp.src(paths.sw)
+        .pipe(uglify())
+        .pipe(gulp.dest('./public'));
 };
 
 const vendorGulp = function() {
-  return gulp.src(paths.vendor)
-    .pipe(uglify())
-    .pipe(gulp.dest('./dist/js/vendors'))
+    return gulp.src(paths.vendor)
+        .pipe(uglify())
+        .pipe(gulp.dest('./public/dist/js/vendors'))
 };
 
 const htmlGulp = function() {
-  return gulp.src(paths.html)
-    .pipe(htmlmin({
-      collapseWhitespace: true,
-      removeComments: true
-    }))
-    .pipe(gulp.dest('./'));
+    return gulp.src(paths.html)
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            removeComments: true
+        }))
+        .pipe(gulp.dest('./public'));
 };
 
 const watchJS = function() {
-  return gulp.watch(paths.script, gulp.series(jsGulp, reload));
+    return gulp.watch(paths.script, gulp.series(jsGulp, reload));
 };
 
 const watchCSS = function() {
-  return gulp.watch(paths.css, gulp.series(gulpCss, reload));
+    return gulp.watch(paths.css, gulp.series(gulpCss, reload));
 };
 
 const watchHTML = function() {
-  return gulp.watch(paths.html, gulp.series(htmlGulp, reload));
+    return gulp.watch(paths.html, gulp.series(htmlGulp, reload));
 };
 
 const server = function() {
-  return connect.server({livereload: true});
+    return connect.server({ livereload: true });
 };
 
 const reload = function() {
-  return gulp.src(paths.html).pipe(connect.reload());
+    return gulp.src(paths.html).pipe(connect.reload());
 };
 
 const watch = gulp.parallel(
-  watchCSS, watchJS, watchHTML
+    watchCSS, watchJS, watchHTML
 );
 
 
